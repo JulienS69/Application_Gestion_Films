254a2d8cdf1a6f8b14175330bcff0712b372653c
JulienS69 <julien.seux69@gmail.com>
2022-05-04 16:29:23 +0200

    Page de connexion avec Splash Screen Animation

---
+++ app/src/main/java/com/example/application_gestion_films/EasterEggActivity.kt
@@ -0,0 +1,32 @@
+package com.example.application_gestion_films
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class EasterEggActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easter_egg)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.easter_egg)
+        backgroundImage.startAnimation(slideAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
---
+++ app/src/main/java/com/example/application_gestion_films/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.application_gestion_films
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+        val email = findViewById<EditText>(R.id.login_email)
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        btn_login.setOnClickListener { login(it) }
+
+    }
+
+    private fun login(view: View){
+        val email = findViewById<EditText>(R.id.login_email).text.toString()
+        val password = findViewById<EditText>(R.id.login_password).text.toString()
+
+        if (email == "" && password == "") {
+            Toast.makeText(this, "Veuillez remplir les différents champs pour vous connecter", Toast.LENGTH_SHORT).show()
+        } else if (email == "" && password != "") {
+            Toast.makeText(this, "Veuillez saisir votre adresse mail", Toast.LENGTH_SHORT).show()
+        } else if (email != "" && password == "") {
+            Toast.makeText(this, "Veuillez saisir un mot de passe", Toast.LENGTH_SHORT).show()
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    val intent2 = Intent(this, EasterEggActivity::class.java)
+                    Toast.makeText(this, "Connecté avec succès !", Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+                    finish()
+                    startActivity(intent2)
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this, "Email ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+/*    fun goToLogin(view: View){
+        val intent= Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }*/
+}
